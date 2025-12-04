0fc0264e22481d32f340427ff034b390df2a20e2
Sarvar <rozimovsravr580@gmail.com>
2025-12-04 09:04:44 +0500

    v4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import About from './about';
 import Plans from './Plans';
 import SignUp from './signUp';
 import SingIn from './signIn';
+import { Fragment } from 'react/jsx-runtime';
+import { useState } from 'react';
+import moon from'./image/moon.jpg'
+import sun from'./image//sun.jpg'
 function App() {
   return (
     <div className="App">
@@ -12,11 +16,18 @@ function App() {
   );
 }
 function Home() {
+const [Navbar, setNavbar] = useState('Navbar')
+
+const change = (e) =>{
+  setNavbar('Navbarbl')
+  console.log(e.target)
+}
+
   return (
    <BrowserRouter>
-   <nav>
-    <Link to="/">Home</Link> <Link to="/about">About</Link> <Link to='/Plans'>Plans</Link> <Link to='/SignIn'>SignIn</Link> <Link to='/SignUp'>SignUp</Link>
-   </nav>
+   <Fragment>
+    <div className= {Navbar}><Link to="/">Home</Link> <Link to="/about">About</Link> <Link to='/Plans'>Plans</Link> <Link to='/SignIn'>SignIn</Link> <Link to='/SignUp'>SignUp</Link> <img src={moon} alt="" onClick={change} /></div>
+   </Fragment>
    <Routes>
     <Route path="/" element={<App />}></Route>
     <Route path='/about' element={<About />}></Route>
