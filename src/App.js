b854c79958d9fa1468fb10da90ed87179d8a17f3
Mohidil <mohidilaliyeva894@gmail.com>
2025-12-04 15:50:41 +0500

    v6

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ import Plans from './Plans';
 import SignUp from './signUp';
 import SingIn from './signIn';
 function App() {
+
   return (
     <div className="App">
-      <h1>Home</h1>
+      <h1>Home</h1>;
     </div>
   );
 }
 function Home() {
+  
   return (
    <BrowserRouter>
    <nav>
-    <Link to="/">Home</Link> <Link to="/about">About</Link> <Link to='/Plans'>Plans</Link> <Link to='/SignIn'>SignIn</Link> <Link to='/SignUp'>SignUp</Link>
+    <Link to="/">Home</Link> <Link to="/about">About</Link>  <Link to='/Plans'>Plans</Link> <Link to='/SignIn'>SignIn</Link> <Link to='/SignUp'>SignUp</Link>
    </nav>
    <Routes>
     <Route path="/" element={<App />}></Route>
