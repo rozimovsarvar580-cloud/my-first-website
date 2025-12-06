43a8e9843beb5d97bf32afed0829ad5bbfa868c6
Mohidil <mohidilaliyeva894@gmail.com>
2025-12-06 17:32:03 +0500

    v10

--- src/App.js
+++ src/App.js
@@ -8,67 +8,74 @@ import { Fragment } from 'react/jsx-runtime';
 import { useState, useEffect } from 'react';
 import moon from './image/moon.jpg';
 import sun from './image/sun.jpg';
+import './LogIn.css'
 function App() {
 
   return (
     <div className="App">
-      <h1>Welcome visitor to our website. Please SingUp: <Link to='/SignUp'>Sign-Up</Link> </h1>
+      <div className="about">
+        <h1>Welcome visitor to our website. If you want use our tool we suggest you <Link to='/SignUp'>Sign-Up</Link> <br /> 
+        and if you more information about our website go to about section
+      </h1>
+         
+      </div>
+    
     </div>
   );
 }
 function Home() {
-   const body = document.getElementsByTagName('body')
-   const colors = () =>{
-    body.style.background-color = '#18181f'
-   }
-   colors()
-   const [Body, SetBody] = useState(() =>{
-    return localStorage.getItem('Body') || 'Body'
-   })
+  const [theme, setTheme] = useState(() =>{
+   return localStorage.getItem('theme') || 'dark'
+  })
    const [Navbar, setNavbar] = useState(() =>{
     return localStorage.getItem('Navbar') || 'Navbar'
    })
    const [image, setImage] = useState(() =>{
-    return localStorage.getItem('image') === 'sun' ? sun: moon
+    return localStorage.getItem('theme') === 'light' ? sun: moon
    })
    useEffect(() =>{
-    localStorage.setItem('Body', Body)
+    document.body.className = theme 
     localStorage.setItem('Navbar', Navbar)
-    localStorage.setItem('image', image === sun ? 'sun' : 'moon')
+    localStorage.setItem('theme', theme)
    })
    const changeNavbar = () =>{
-    if(Navbar === 'Navbar'){
-      SetBody('')
+    if(theme === 'dark'){
+      setTheme('light')
       setNavbar('NavbarLight')
       setImage(sun)
     }else{
-     
+     setTheme('dark')
       setNavbar('Navbar')
       setImage(moon)
     }
+   }
+   const visiting = (e) =>{
+   console.log(e)
    }
   return (
    
    <BrowserRouter>
    <Fragment>
     <div className={Navbar}>
-      <Link to="/">Home</Link>
-     <Link to="/about">About</Link> 
-     <Link to='/Plans'>Plans</Link> 
-     <Link to='/SignIn'>Log-In</Link> 
-     <Link to='/SignUp'>Sign-Up</Link>
+      <Link to="/Home" onClick={visiting}>Home</Link>
+     <Link to="/about"  onClick={visiting}>About</Link> 
+     <Link to='/Plans'  onClick={visiting}>Plans</Link> 
+     <Link to='/SignIn'  onClick={visiting}>Log-In</Link> 
+     <Link to='/SignUp'  onClick={visiting}>Sign-Up</Link>
      <img src={image} alt="" className='img' onClick={changeNavbar} />
      </div>
    </Fragment>
    <Routes>
-    <Route path="/" element={<App />}></Route>
+    <Route path="/Home" element={<App />}></Route>
     <Route path='/about' element={<About />}></Route>
     <Route path='/Plans' element={<Plans />}></Route>
     <Route path='/SignIn' element={<SingIn />}></Route>
     <Route path='/SignUp' element={<SignUp />}></Route>
    </Routes>
-   </BrowserRouter>
+   </BrowserRouter> 
+   
   )
+  
 }
 
 export default Home;
