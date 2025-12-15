10b4ef167eb50ea201d50b6f47205a288f9ca771
Sarvar <rozimovsravr580@gmail.com>
2025-12-15 10:40:32 +0500

    v15

--- src/tool.js
+++ src/tool.js
@@ -1,26 +1,41 @@
-import { useState, } from 'react';
+import { useState, useEffect } from 'react';
 import './todo.css'
+import bin  from'./image/bin.jpg'
  function Plans() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState(() =>{
+   
+    const saved = localStorage.getItem('todos') 
+    return saved ? JSON.parse(saved): []
+  })
+  const [input, setInput] = useState('')
+  useEffect(() =>{
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
  const AddTodo = () =>{
-  setTodos([...todos, `Todo ${todos.length + 1}`])
+  if(input.trim() === '') return
+  setTodos([...todos, input])
+  setInput('')
  }
- const value = () =>{
-  
- }
+ 
   return (
     <div>
       <div className='todo'> 
-        
       <ul>
         {todos.map((todo, index) =>(
-          <li key={index}>{todo}</li>
+          <li key={index}>{todo} <img src={bin} alt="" /></li>
         ))}
       </ul>
-      <input type="text" placeholder='type what you want do' id='input'/><br />
+      <input 
+      type="text"
+       placeholder='type what you want do'
+        id='input'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        /><br />
      <button onClick={AddTodo}>Add</button>
      </div>
     </div>
   );
+  
 }
 export default Plans
